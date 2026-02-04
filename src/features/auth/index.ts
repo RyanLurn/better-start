d4a90f18d6b3873ec0a220d3a418f3476636fbea
Ryan Luong <lumintriet@gmail.com>
2026-02-04 16:58:40 +0700

    feat(auth): mount Better Auth API to TanStack Start route.

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,4 @@
+import { tanstackStartCookies } from "better-auth/tanstack-start";
 import { betterAuth } from "better-auth";
 
 import { dbOptions } from "@/features/auth/config/db";
@@ -12,4 +13,5 @@ export const auth = betterAuth({
   secret: serverEnv.BETTER_AUTH_SECRET,
   baseURL: serverEnv.BETTER_AUTH_URL,
   ...dbOptions,
+  plugins: [tanstackStartCookies()], // TanStack Start cookie plugin must come last
 });
