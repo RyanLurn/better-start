bfd200663c455d3ff59720129661a75e5d62c34f
Ryan Luong <lumintriet@gmail.com>
2026-02-04 16:37:36 +0700

    feat(auth): add env vars for Better Auth.

--- src/lib/env/server.ts
+++ src/lib/env/server.ts
@@ -4,6 +4,8 @@ import { z } from "zod";
 export const serverEnv = createEnv({
   server: {
     NEON_POOLED_CONNECTION_STRING: z.url(),
+    BETTER_AUTH_SECRET: z.base64(),
+    BETTER_AUTH_URL: z.url(),
   },
   runtimeEnv: process.env,
 });
