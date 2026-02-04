7b223887ffcf04bdeb053106506a9a6e630d44b7
Ryan Luong <lumintriet@gmail.com>
2026-02-04 16:44:18 +0700

    feat(auth): add env vars to auth instance.

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,10 +1,13 @@
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { betterAuth } from "better-auth";
 
+import { serverEnv } from "@/lib/env/server";
 import { db } from "@/db";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  secret: serverEnv.BETTER_AUTH_SECRET,
+  baseURL: serverEnv.BETTER_AUTH_URL,
 });
