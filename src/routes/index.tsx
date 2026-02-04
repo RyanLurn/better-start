829c070f44e80ce7941f909c871ce10327dee661
Ryan Luong <lumintriet@gmail.com>
2026-02-04 08:56:25 +0700

    feat(ui): add Button component from shadcn/ui.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { Button } from "@/components/ui/button";
+
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
     <div className="flex h-dvh flex-col items-center justify-center gap-y-2">
-      <h1>Better Start</h1>
+      <h1 className="text-2xl font-bold">Better Start</h1>
+      <Button>Button</Button>
     </div>
   );
 }
