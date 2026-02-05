ce70dedb00125b0ef9aec5126300445d5921bbef
Ryan Luong <lumintriet@gmail.com>
2026-02-05 11:19:28 +0700

    feat(auth): send verification email.

--- src/features/auth/config/magic-link.ts
+++ src/features/auth/config/magic-link.ts
@@ -1,11 +1,19 @@
 import { magicLink } from "better-auth/plugins";
 
+import type { Email } from "@/features/emails/types";
+
+import { sendEmail } from "@/features/emails/send";
+
 export const magicLinkPlugin = magicLink({
-  sendMagicLink: async ({ email, token, url }, ctx) => {
-    // send email to user
-    await Promise.resolve(() => {
-      console.log("Magic link sent to:", email, "URL:", url, "Token:", token);
-      console.log("Context:", ctx);
-    });
+  sendMagicLink: async ({ email: emailAddress, url }) => {
+    const email: Email = {
+      html: `<p>Click the link below to sign in: <a href="${url}">${url}</a></p>`,
+      text: `Click the link below to sign in: ${url}`,
+      from: "support@betterstart.com",
+      subject: "Sign in link",
+      to: emailAddress,
+    };
+
+    await sendEmail({ email });
   },
 });
