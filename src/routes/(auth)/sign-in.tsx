467d053f20709b79e6a06e17bb6e59b87d57a975
Ryan Luong <lumintriet@gmail.com>
2026-02-07 08:44:03 +0700

    feat(auth): add SignInForm component to sign-in route.

--- src/routes/(auth)/sign-in.tsx
+++ src/routes/(auth)/sign-in.tsx
@@ -1,9 +1,15 @@
 import { createFileRoute } from "@tanstack/react-router";
 
+import { SignInForm } from "@/features/auth/components/forms/sign-in";
+
 export const Route = createFileRoute("/(auth)/sign-in")({
-  component: RouteComponent,
+  component: SignIn,
 });
 
-function RouteComponent() {
-  return <div>Hello "/(auth)/sign-in"!</div>;
+function SignIn() {
+  return (
+    <div className="flex h-dvh flex-col items-center justify-center">
+      <SignInForm />
+    </div>
+  );
 }
